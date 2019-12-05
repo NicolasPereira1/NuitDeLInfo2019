7b7a3f94d891ad96ea312585cdd153cca2ef96d5
landscht <tony.landschoot@outlook.com>
2019-12-05 19:40:20 +0100

    front

---
+++ front/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-app-bar
+            v-if="display"
+            app
+            color="red"
+            dark
+    >
+        <div class="d-flex align-center">
+           Nom du site{{nom}}
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target="_blank"
+                text
+        >
+            <span class="mr-2">Se connecter</span>
+            <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+    import UserService from '../services/user-service'
+    export default {
+        name: "Header",
+        data : () => ({
+            display : true,
+            nom : UserService.nom,
+            prenom : UserService.prenom
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/services/user-service.js
@@ -0,0 +1,4 @@
+export default {
+    nom : "landschoot",
+    prenom : "tony"
+}
