fa3120b6d314fc62f23b0624b1d4be28568d7f02
landscht <tony.landschoot@outlook.com>
2019-12-05 20:51:19 +0100

    front login

--- front/src/components/Login.vue
+++ front/src/components/Login.vue
@@ -1,8 +1,37 @@
 <template>
-    <h1>Log in</h1>
-    <v-form>
-        <v-text-field></v-text-field>
-    </v-form>
+    <v-container>
+        <v-flex xs6>
+            <v-form
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation
+            >
+                <v-text-field
+                        v-model="name"
+                        :counter="10"
+                        :rules="nameRules"
+                        label="Name"
+                        required
+                ></v-text-field>
+
+                <v-text-field
+                        v-model="email"
+                        :rules="emailRules"
+                        label="E-mail"
+                        required
+                ></v-text-field>
+
+                <v-btn
+                        :disabled="!valid"
+                        color="primary"
+                        class="mr-4"
+                        @click="validate"
+                >
+                    Se connecter
+                </v-btn>
+            </v-form>
+        </v-flex>
+    </v-container>
 </template>
 
 <script>
@@ -11,13 +40,39 @@ export default {
   name: 'Login',
   components: {
   },
-  data: () => ({
-  }),
-  methods : {
+    data: () => ({
+        valid: true,
+        name: '',
+        nameRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        ],
+        email: '',
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+        select: null,
+    }),
 
-  }
+    methods: {
+        validate () {
+            if (this.$refs.form.validate()) {
+                this.snackbar = true
+            }
+        },
+        reset () {
+            this.$refs.form.reset()
+        },
+        resetValidation () {
+            this.$refs.form.resetValidation()
+        },
+    },
 };
-
-
-
 </script>
+
+<style>
+    .formulaire {
+        width: 50%;
+    }
+</style>
